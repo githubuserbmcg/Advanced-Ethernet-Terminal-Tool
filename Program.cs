681bd8bcfaac95e5e75557c02f7753d234190fbb
githubuserbmcg <68115597+githubuserbmcg@users.noreply.github.com>
2022-09-28 16:54:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advanced_Ethernet_Terminal_Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
